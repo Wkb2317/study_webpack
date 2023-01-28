c32cf49cd1ade9ccd9c8f674b64575bed3ccc9ba
Wkb2317 <2576267399@qq.com>
2023-01-28 22:44:31 +0800

    feat: code_split 多入口

---
+++ config/code_split.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    count: path.resolve(__dirname, '../src/js/count.js'),
+    math: path.resolve(__dirname, '../src/js/math.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
+    filename: '[name]-[hash:6].js',
+  },
+  module: {
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'),
+    }),
+  ],
+}
