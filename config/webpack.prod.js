0afaf86d410c3a4bec856287f6526f2403c60aec
Wkb2317 <2576267399@qq.com>
2023-02-06 21:06:23 +0800

    preload and prefetch

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,7 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin')
+// const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 
 // cpu核数量
 const threads = os.cpus().length
@@ -28,70 +29,50 @@ module.exports = {
   // loader
   module: {
     rules: [{
-      oneOf: [
-        {
-          test: /\.css$/i,
-          use: getStyleLoader(),
-        },
-        {
-          test: /\.less$/i,
-          use: getStyleLoader('less-loader'),
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: getStyleLoader('sass-loader'),
-        },
-        {
-          test: /\.styl$/,
-          use: getStyleLoader('stylus-loader'),
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          type: 'asset',
-          parser: {
-            dataUrlCondition: {
-              maxSize: 10 * 1024, // 图片转base64
-            },
+      oneOf: [{
+        test: /\.css$/i, use: getStyleLoader(),
+      }, {
+        test: /\.less$/i, use: getStyleLoader('less-loader'),
+      }, {
+        test: /\.s[ac]ss$/i, use: getStyleLoader('sass-loader'),
+      }, {
+        test: /\.styl$/, use: getStyleLoader('stylus-loader'),
+      }, {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 图片转base64
           },
-          // generator: {
-          //   // 输出文件名称
-          //   filename: 'img/[hash:10][ext][query]',
+        }, // generator: {
+        //   // 输出文件名称
+        //   filename: 'img/[hash:10][ext][query]',
+        // },
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf|mp3|mp4)$/i, type: 'asset/resource', // generator: {
+        //   filename: 'fonts/[hash:10][ext][query]',
+        // },
+      }, {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'thread-loader',
+          options: {
+            workers: threads,
+          },
+        }, {
+          loader: 'babel-loader', // options: {
+          //   presets: ['@babel/preset-env'],
           // },
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf|mp3|mp4)$/i,
-          type: 'asset/resource',
-          // generator: {
-          //   filename: 'fonts/[hash:10][ext][query]',
-          // },
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: [
-            {
-              loader: 'thread-loader',
-              options: {
-                workers: threads,
-              },
-            },
-            {
-              loader: 'babel-loader',
-              // options: {
-              //   presets: ['@babel/preset-env'],
-              // },
-              options: {
-                cacheDirectory: true, // 开启缓存
-                cacheCompression: false, // 开启压缩
-                plugins: ['@babel/plugin-transform-runtime'], // 减少代码体积
-              },
-            },
-          ],
-        },
-      ],
+          options: {
+            cacheDirectory: true, // 开启缓存
+            cacheCompression: false, // 开启压缩
+            plugins: ['@babel/plugin-transform-runtime'], // 减少代码体积
+          },
+        }],
+      }],
     }],
-  },
-  // 插件
+  }, // 插件
   plugins: [
     new ESLintPlugin({
       context: path.resolve(__dirname, '../src'),
@@ -103,18 +84,21 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html'), // 模板路径
     }),
+    new PreloadWebpackPlugin({
+      // as: 'script',
+      // rel: 'preload',
+      rel: 'prefetch',
+    }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].css', //  CSS 提取到单独的文件
       chunkFilename: 'css/[name].chunk.css',
-    }),
-    new CssMinimizerPlugin(), // 压缩css
+    }), new CssMinimizerPlugin(), // 压缩css
     new TerserPlugin({
       parallel: threads,
     }),
   ],
   optimization: {
-    minimizer: [
-      // 压缩图片
+    minimizer: [// 压缩图片
       // 没用，增加打包时间，依赖还下载不下来
       // new ImageMinimizerPlugin({
       //   minimizer: {
