06cfeb29efd1da8bfda67d6012864e839d498310
Wkb2317 <2576267399@qq.com>
2023-02-06 22:24:53 +0800

    pwa

--- src/main.js
+++ src/main.js
@@ -31,3 +31,14 @@ if (module.hot) {
 const pro = new Promise((resolve, reject) => {
   resolve(1)
 })
+
+console.log('serviceWorker' in navigator)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('SW registered: ', registration)
+    }).catch((registrationError) => {
+      console.log('SW registration failed: ', registrationError)
+    })
+  })
+}
